31b6ec40840c92a9f4cdb0ef16237c1fe44864eb
knassar702 <root@localhost.localdomain>
2019-08-20 15:58:15 +0200

    wp-penetest

---
+++ wp-user.py
@@ -0,0 +1,18 @@
+import requests,sys
+try:
+	url=sys.argv[1]+'/wp-json/wp/v2/users/'
+except:
+	print('[-] Error ..')
+	sys.exit()
+try:
+	r=requests.get(url)
+except:
+	print('[-] Error ..')
+	sys.exit()
+j = r.json()
+print('\n# => @knassar702')
+for i in j:
+	print('----------------')
+	print('[*] Name : {}\n[*] ID   : {}\n[*] User : {}'.format(i['name'],i['id'],i['slug']))
+print('----------------\n')
+# Coded By : Khaled Nassar @knassar702
